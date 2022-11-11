b522d5137012a48efe15217e0ac54f18e07deedd
Nathan Walker <walkerrunpdx@gmail.com>
2022-11-11 12:28:22 -0800

    chore: cleanup

--- apps/demo-angular/src/main.ts
+++ apps/demo-angular/src/main.ts
@@ -1,8 +1,8 @@
 import { runNativeScriptAngularApp, platformNativeScript } from '@nativescript/angular';
 import { AppModule } from './app.module';
 
-import { StorybookDevice } from '@nativescript/storybook-device';
-StorybookDevice.init();
+// import { StorybookDevice } from '@nativescript/storybook-device';
+// StorybookDevice.init();
 
 // this will go to the .stories file
 // import { StorybookWeb } from '@nativescript/storybook-web';
